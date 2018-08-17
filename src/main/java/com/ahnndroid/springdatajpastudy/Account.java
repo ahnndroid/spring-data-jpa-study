98f65dfcc9f1f1cc0a35418020d5fd29d9ca7c9f
Hyungjin An <super702@gmail.com>
2018-08-17 17:05:17 +0900

    Account 엔티티 일부 수정
    - nullable 설정
    - unique 설정
    - 타임스탬프 설정

--- src/main/java/com/ahnndroid/springdatajpastudy/Account.java
+++ src/main/java/com/ahnndroid/springdatajpastudy/Account.java
@@ -1,8 +1,14 @@
 package com.ahnndroid.springdatajpastudy;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 import lombok.Data;
 
@@ -14,7 +20,15 @@ public class Account {
 	@GeneratedValue
 	private Long id;
 	
+	@Column(nullable = false, unique = true)
 	private String username;
 	
+	@Column(nullable = false)
 	private String password;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date created = new Date();
+	
+	@Transient
+	private String no;
 }
